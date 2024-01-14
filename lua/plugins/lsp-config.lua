44347fea64a8e4af2706adf2685479ff8b347881
yash karakoti <yash_karakoti>
2024-01-15 03:17:57 +0530

    config refactoring

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -8,7 +8,7 @@ return {
     'williamboman/mason-lspconfig.nvim',
     config = function ()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "csharp_ls", "clangd", "tsserver" }
+        ensure_installed = { "lua_ls", "clangd", "tsserver" }
       })
     end
   },
@@ -16,17 +16,13 @@ return {
     "neovim/nvim-lspconfig",
     config = function()
       local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
-      lspconfig.clangd.setup({})
-      lspconfig.csharp_ls.setup({})
-      lspconfig.tsserver.setup({})
+      lspconfig.lua_ls.setup{}
+      lspconfig.clangd.setup{}
+      lspconfig.tsserver.setup{}
 
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
     end
-  }
+  },
 }
-
-
-
